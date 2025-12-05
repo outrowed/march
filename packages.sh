fc2d8cd760a90b4fa4e3edc39fede8d18987e3ea
outrowed <outrowed@gmail.com>
2025-12-05 21:31:13 +0700

    revamp packages.sh with docs and improved list
    additions:
    + gpftfdisk
    + btop
    + tree
    + speedtest-cli
    + archiso
    
    removal:
    - htop
    - timeshift
    - timeshift-systemd-timer
    
    moved a lot of DE related packages to post-installation IPACMAN_PACKAGES from IPACSTRAP_PACKAGES, mostly because they are not needed in the base Arch Linux installation
    
    improved documentation for each package list variables

--- packages.sh
+++ packages.sh
@@ -1,30 +1,41 @@
 #!/usr/bin/bash
 
-## Installation packages
+## Pacstrap packages
+# essential packages for "comfortable but minimal" base Arch Linux installation
+# -- that may be used for rescue or recovery mode
+# anything that would make sense for DE is not included here, instead in later IPACMAN_PACKAGES, IAUR_PACKAGES, or ILATE_PACKAGES
+# minimize the download/installation time
 IPACSTRAP_PACKAGES=(
-    ## Base System & Microcode
+    ## System + microcode
     base
     linux
     linux-firmware
-    base-devel
     intel-ucode
     amd-ucode
+    # Extra
+    base-devel
+    linux-headers
 
-    ## Partition & Filesystem Tools
+    ## Partition + FS
+    # EFI
     efibootmgr
     efivar
+    # FS
     btrfs-progs
     ntfs-3g
     dosfstools
     e2fsprogs
+    # Partition
     parted
+    gptfdisk
 
-    ## Admin & System Utilities
+    ## Admin
     sudo
     openssh
     openssl
     git
     curl
+    wget
     nano
     vim
     man-db
@@ -32,47 +43,44 @@ IPACSTRAP_PACKAGES=(
     bash-completion
     less
     gzip
-    gparted
     xz
     zip
     unzip
-    linux-headers
-    htop
-    fwupd           # Firmware updates
+    btop            # alternative to htop
+    fwupd           # firmware updates
     fastfetch
     rsync
-    smartmontools
-    timeshift
-    plocate         # "everything" in linux
-    plymouth        # boot startup
     zsh
     zram-generator
-    arch-audit-gtk
+    # FS listing / finder
+    tree
+    plocate         # "everything" file finder in linux
 
     ## Network
+    # Core
     systemd-resolvconf
     networkmanager
-    wireguard-tools
-    ufw                     # firewall
-    network-manager-applet
+    ufw                 # firewall
+    # Utils
     inetutils
     openbsd-netcat
-    wget
-    kdenetwork-filesharing
+    speedtest-cli
+    # Wireless
     iwd
     wireless_tools
     wpa_supplicant
-    bluez                   # bluetooth support
+    # Bluetooth
+    bluez               # bluetooth support
     bluez-utils
-    avahi                   # Network discovery (mDNS/Bonjour)
-    nss-mdns                # Hostname resolution for .local domains
+    # Extra
+    wireguard-tools
 
-    ## Pacman Utilities
+    ## Pacman extra
     pacman-contrib
     reflector
     arch-audit
 
-    ## Development Tools
+    ## Devel
     python
     shellcheck
     gcc
@@ -83,11 +91,104 @@ IPACSTRAP_PACKAGES=(
     jq
     go
 
-    ## Multimedia & Audio
+    ## Multimedia
+    ffmpeg
+
+    ## Audio firmware
+    sof-firmware
+    alsa-firmware
+
+    ## Generic GPU
+    mesa
+
+    ## NVIDIA
+    nvidia
+    nvidia-utils
+    nvidia-settings
+
+    ## Zsh extra
+    zsh-completions
+    zsh-syntax-highlighting
+    zsh-autosuggestions
+    grml-zsh-config         # Recommendation per https://wiki.archlinux.org/title/Zsh#Sample_.zshrc_files
+)
+
+## Pacman packages
+# packages installed after the base system installation
+# usually this is where DE and graphical related things are installed
+IPACMAN_PACKAGES=(
+    ## Generic GPU
+    vulkan-icd-loader
+
+    ## NVIDIA
+    nvidia-prime
+    # ex: cuda -- added in ILATE_PACKAGES
+
+    ## Intel
+    intel-media-driver
+    vulkan-intel
+
+    ## AMD
+    libva-mesa-driver
+    mesa-vdpau
+    vulkan-radeon
+
+    ## Thunderbolt
+    bolt
+
+    ## Admin
+    gparted
+    smartmontools
+    nvtop
+    iotop
+    # ex: timeshift
+
+    ## Desktop Environment (KDE Plasma)
+    # Core
+    sddm
+    plasma-meta
+    xorg-xwayland
+    # Extra
+    kde-accessibility
+    kde-system
+    kde-utilities
+    kde-graphics
+    # ex: kde-network
+    # ex: kde-office
+    # ex: kde-multimedia
+    # KCM
+    sddm-kcm
+    plymouth-kcm
+    # KIO
+    kio-admin
+    kio-extras
+    kio-gdrive
+    kio-zeroconf
+    # Theme
+    breeze
+    # Remote
+    krdc
+    # Plugin + integration
+    kdenetwork-filesharing
+    plasma-thunderbolt
+
+    ## Generic DE
+    plymouth            # boot startup
+    xdg-utils
+    trash-cli
+    iio-sensor-proxy
+    # Extra
+    network-manager-applet
+    arch-audit-gtk
+
+    ## Audio player
     mpv
     vlc
-    ffmpeg
+
+    ## Multimedia
     ffmpegthumbs
+
+    ## Audio
     pipewire
     wireplumber
     pipewire-jack
@@ -97,87 +198,28 @@ IPACSTRAP_PACKAGES=(
     alsa-utils
     gst-plugin-pipewire
     pavucontrol
-    sof-firmware
-    alsa-firmware
 
-    ## Generic GPU Drivers
-    mesa
-
-    ## NVIDIA Drivers
-    nvidia
-    nvidia-utils
-    nvidia-settings
-
-    ## Zsh Plugins / Configs
-    zsh-completions
-    zsh-syntax-highlighting
-    zsh-autosuggestions
-    grml-zsh-config         # Recommendation per https://wiki.archlinux.org/title/Zsh#Sample_.zshrc_files
-)
-
-## Post install Pacman packages
-# massive packages that are not that "important" for the base installation
-IPACMAN_PACKAGES=(
-    ## System Utilities
-    nvtop
-    iotop
-
-    ## Generic GPU Drivers
-    vulkan-icd-loader
-
-    ## NVIDIA Drivers
-    nvidia-prime
-    cuda            # here's the big boi
-
-    ## Intel Drivers
-    intel-media-driver
-    vulkan-intel
-
-    ## AMD Drivers
-    libva-mesa-driver
-    mesa-vdpau
-    vulkan-radeon
-
-    ## Desktop Environment (KDE Plasma)
-    xorg-xwayland
-    sddm
-    sddm-kcm
-    plymouth-kcm
-    plasma-meta
-    kde-accessibility
-    kde-system
-    kde-utilities
-    kio-admin
-    kio-extras
-    kio-gdrive
-    kio-zeroconf
-    krdc
-    # kde-network
-    # kde-office
-    # kde-multimedia
-    kde-graphics
-    breeze
-    xdg-utils
-    trash-cli
-    iio-sensor-proxy
-    bolt
-    plasma-thunderbolt
+    ## Network
+    avahi       # Network discovery (mDNS/Bonjour)
+    nss-mdns    # Hostname resolution for .local domains
 
     ## Localization
     hunspell
     hyphen
 
-    ## Development Tools (post install)
+    ## Devel
     nodejs
     npm
     pnpm
 
-    ## User Applications
-    flatpak
-    bat
-    fuse3
-    syncthing
-    yt-dlp
+    ## Package manager
+    flatpak     # mostly user-space GUI-based applications
+
+    ## User software
+    syncthing   # syncthing
+    yt-dlp      # youtube download
+    bat         # cat clone
+    fuse3       # Filesystem in Userspace -- mount filesystem without root
 
     ## Fonts
     ttf-dejavu
@@ -191,36 +233,38 @@ IPACMAN_PACKAGES=(
     otf-font-awesome
 
     ## Virtualization
-    qemu-desktop
-    virt-manager
-    libvirt
-    edk2-ovmf
-    dnsmasq
-    virt-viewer
-    dmidecode
+    qemu-desktop    # QEMU setup for desktop environment
+    virt-manager    # GUI front end for virtualization
+    libvirt         # virtualizatin API
+    edk2-ovmf       # UEFI firmware for virtual machines
+    virt-viewer     # graphical display required by virt-manager
+    
+    ## Virtualization utility
+    dnsmasq         # DNS + DHCP server
+    dmidecode       # system hardware report via SMBIOS/DMI standard
 
-    ## Printing Support
-    cups                            # The core printing system
-    cups-pdf                        # Print to PDF support
-    gutenprint                      # High-quality drivers for Canon, Epson, etc.
-    foomatic-db-gutenprint-ppds     # PPD files for Gutenprint
-    hplip                           # Drivers for HP printers
+    ## Print support
+    cups                        # The core printing system
+    cups-pdf                    # Print to PDF support
+    gutenprint                  # High-quality drivers for Canon, Epson, etc.
+    foomatic-db-gutenprint-ppds # PPD files for Gutenprint
+    hplip                       # Drivers for HP printers
 )
 
 ## AUR packages
+# run with no password sudo
 IAUR_PACKAGES=(
-    ## System Utilities
-    timeshift-systemd-timer
-    topgrade
-    needrestart
+    needrestart # automatically restart daemon after library update
+    # ex: timeshift-systemd-timer
 )
 
-## Late AUR / Pacman packages (need user to build)
+## Late AUR / Pacman packages
+# some packages have very long compile time and thus needs to be deferred / ran in background
 ILATE_PACKAGES=(
-    ## Utilities
-    maliit-keyboard
-    ventoy-bin
-
-    ## Applications
-    sunshine-bin    # moonlight
+    cuda            # big boi NVIDIA driver
+    maliit-keyboard # virtual keyboard
+    topgrade        # all-in-one universal system upgrade
+    sunshine-bin    # remote server
+    ventoy-bin      # ventoy bootloader
+    archiso         # archiso creation
 )
