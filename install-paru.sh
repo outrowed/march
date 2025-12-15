415062007686808f6a6ed9887c22d624248b9dff
outrowed <outrowed@gmail.com>
2025-12-15 23:12:25 +0700

    migration to YAY as paru is not working right now (libalpm)

--- install-paru.sh
+++ install-paru.sh
@@ -2,17 +2,20 @@
 
 . "$(dirname ${BASH_SOURCE[0]})"/common.sh
 
-# Install paru's build dependencies (Rust) into the new system
-retry arch-chroot /mnt pacman -S --noconfirm --needed rust
+# Install yay's build dependencies (requires Go)
+retry arch-chroot /mnt pacman -S --noconfirm --needed go
 
 # Run the build process as the super user, not as root
 # We use 'sudo -u' for this.
 autosudo "$ISUPER_USER" /mnt retry arch-chroot /mnt sudo -u "$1" bash -c "
     cd /tmp
-    git clone https://aur.archlinux.org/paru.git /tmp/paru-build
-    cd /tmp/paru-build
+    git clone https://aur.archlinux.org/yay-bin.git /tmp/yay-build
+    cd /tmp/yay-build
     makepkg -si --noconfirm
 "
 
 # Clean up the build directory
-arch-chroot /mnt rm -rf /tmp/paru-build
+arch-chroot /mnt rm -rf /tmp/yay-build
+
+# Provide compatibility alias: use yay but keep 'paru' command working
+arch-chroot /mnt ln -sf /usr/bin/yay /usr/bin/paru
